b52ba0fff334c47b6756ce534241fc330ea770f6
Jeroen Claassens <support@favware.tech>
2021-06-09 15:20:31 +0200

    fix: fixed import of Nullish

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
-import { isNullish, Nullish } from '@sapphire/utilities';
+import type { Nullish } from '@sapphire/utilities';
+import { isNullish } from '@sapphire/utilities';
 
 export function isNullishOrEmpty(value: unknown): value is Nullish | '' {
 	return value === '' || isNullish(value);
