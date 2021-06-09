b71aea1a8a08954e9110b76adb688181e3330181
Jeroen Claassens <support@favware.tech>
2021-06-09 20:03:34 +0200

    chore: please no changes this time.

--- api/lib/App.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Probot } from 'probot';
-declare const _default: (app: Probot) => void;
-export default _default;
-//# sourceMappingURL=App.d.ts.map
--- api/lib/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContinuousDeliveryWorkflow = exports.rootFolder = void 0;
-const path_1 = require("path");
-exports.rootFolder = path_1.join(__dirname, '..', '..');
-exports.ContinuousDeliveryWorkflow = 'continuous-delivery.yml';
-//# sourceMappingURL=constants.js.map
