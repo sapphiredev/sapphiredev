3bc377450bb5ea09d34e59ca42ea61862535c766
Jeroen Claassens <support@favware.tech>
2021-06-09 20:51:20 +0200

    refactor: remove try/catch, vercel doesn't log errors anyway

--- src/App.ts
+++ src/App.ts
@@ -26,32 +26,24 @@ export default (app: Probot) => {
 			const issueBodyLower = context.payload.comment.body.toLowerCase();
 			const workflowUrl = `https://github.com/${context.payload.repository.full_name}/actions/workflows/${ContinuousDeliveryWorkflow}`;
 
-			try {
-				const fullPrData = await context.octokit.pulls.get(context.pullRequest());
+			const fullPrData = await context.octokit.pulls.get(context.pullRequest());
 
-				if (issueBodyLower.includes('@sapphiredev deploy')) {
-					try {
-						await context.octokit.actions.createWorkflowDispatch({
-							workflow_id: ContinuousDeliveryWorkflow,
-							owner: context.payload.repository.owner.name ?? 'sapphiredev',
-							repo: context.payload.repository.name,
-							ref: fullPrData.data.head.ref
-						});
+			if (issueBodyLower.includes('@sapphiredev deploy')) {
+				await context.octokit.actions.createWorkflowDispatch({
+					workflow_id: ContinuousDeliveryWorkflow,
+					owner: context.payload.repository.owner.name ?? 'sapphiredev',
+					repo: context.payload.repository.name,
+					ref: fullPrData.data.head.ref
+				});
 
-						const replyMessage = context.issue({
-							body: [
-								`Heya @${context.payload.sender.login}, I've started to run the deployment workflow on this PR.`,
-								`You can monitor the build [here](${workflowUrl})!`
-							].join(' ')
-						});
+				const replyMessage = context.issue({
+					body: [
+						`Heya @${context.payload.sender.login}, I've started to run the deployment workflow on this PR.`,
+						`You can monitor the build [here](${workflowUrl})!`
+					].join(' ')
+				});
 
-						await context.octokit.issues.createComment(replyMessage);
-					} catch (error) {
-						context.log.fatal(error);
-					}
-				}
-			} catch (error) {
-				context.log.fatal(error);
+				await context.octokit.issues.createComment(replyMessage);
 			}
 		}
 	});
