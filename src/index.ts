10568396867ead9c8f2fe4d4cdd8b5cd90de7b29
Jeroen Claassens <jeroen.claassens@rws.nl>
2023-08-29 13:18:04 +0200

    chore: update dev deps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { config } from 'dotenv-cra';
+import { setup } from '@skyra/env-utilities';
 import { join } from 'path';
 import { createNodeMiddleware, createProbot } from 'probot';
 import app from './app';
 
-config({
+setup({
 	path: process.env.NODE_ENV === 'production' ? join(__dirname, '.env') : join(__dirname, '..', '.env')
 });
 
